3b67d9d7b65a92036a1917017a5ea70b377a22ed
Linus Areng <linus.areng@intellecta.se>
2020-10-14 21:22:31 +0200

    clueless

--- LAstandings/LAstandings.js
+++ LAstandings/LAstandings.js
@@ -47,12 +47,151 @@ define(['SIMRacingApps', 'css!widgets/LAstandings/LAstandings', 'widgets/CarNumb
 
                             $scope.sraRelativeCar = "REFERENCE";
 
+                            // start flag edits
+                            $scope.sessionType = '';
+                            $scope.CAUTIONBLINKDURATION = 30000;
+                            $scope.timeStarted = 0;
+                            $scope.blink = false;
+                            $scope.update = function() {
+                                var value = $scope.data.Session.Messages.Value + "" + $scope.data.Car.REFERENCE.Messages.Value;
+                                var flags = [];
+                                $scope.blink = false;
+
+                                //process the flag in importance order. Only the first 2 will get displayed.
+                                if ($scope.data.Session.IsRedFlag.Value) {
+                                    flags.push("red");
+                                }
+                                if ($scope.data.Car.REFERENCE.IsBlackFlag.Value) {
+                                    flags.push("black");
+                                }
+                                if ($scope.data.Car.REFERENCE.IsBlueFlag.Value) {
+                                    flags.push("blue");
+                                }
+                                if ($scope.data.Session.IsGreenFlag.Value) {
+                                    flags.push("green");
+                                }
+                                if ($scope.data.Session.IsWhiteFlag.Value) {
+                                    flags.push("white");
+                                }
+                                if ($scope.data.Session.IsCheckeredFlag.Value) {
+                                    flags.push("checkered");
+                                }
+                                if ($scope.data.Session.IsCautionFlag.Value || $scope.data.Car.REFERENCE.IsYellowFlag.Value) {
+                                    flags.push("yellow");
+                                    if ($scope.timeStarted == 0)
+                                        $scope.timeStarted = Date.now();
+                                        
+                                    if (Date.now() < ($scope.timeStarted + $scope.CAUTIONBLINKDURATION))
+                                        $scope.blink = true;
+                                }
+                                else {
+                                    $scope.timeStarted = 0;
+                                }
+                                if ($scope.data.Session.IsCrossedFlag.Value) {
+                                    //TODO: Find a picture of a crossed flag
+                                }
+                                if (value.indexOf(";REPAIR;") != -1) {
+                                    flags.push("repair");
+                                }
+                                
+                                if (flags.length > 0) {
+                                    if ($scope.displayLeft)
+                                        $scope.image  = "SIMRacingApps-Widget-FlagsBar-image-left  SIMRacingApps-Widget-FlagsBar-left-" +flags[0];
+                                    else {
+                                        if (flags.length > 1)
+                                            $scope.image = "SIMRacingApps-Widget-FlagsBar-image-right SIMRacingApps-Widget-FlagsBar-right-"+flags[1];
+                                        else
+                                            $scope.image = "SIMRacingApps-Widget-FlagsBar-image-right SIMRacingApps-Widget-FlagsBar-right-"+flags[0];
+                                    }
+                                }
+                                else {
+                                    //if no flag, then display green, if it has already been displayed
+                                    if ($scope.displayGreen && $scope.data.Session.Status.Value == "GREEN") {
+                                        if ($scope.displayLeft)
+                                            $scope.image  = "SIMRacingApps-Widget-FlagsBar-image-left SIMRacingApps-Widget-FlagsBar-left-green";
+                                        else
+                                            $scope.image  = "SIMRacingApps-Widget-FlagsBar-image-right SIMRacingApps-Widget-FlagsBar-right-green";
+                                    }
+                                    else {
+                                        //turn off flags
+                                        if ($scope.displayLeft)
+                                            $scope.image  = "SIMRacingApps-Widget-FlagsBar-image-left";
+                                        else
+                                            $scope.image  = "SIMRacingApps-Widget-FlagsBar-image-right";
+                                    }
+                                }
+
+                                if ($scope.blink) {
+                                    if ($scope._blink == null) {
+                                        $scope._blink = $interval(function() {
+                                            if ($scope.flagsVisibility == 'inherit') {
+                                                $scope.flagsVisibility = 'hidden';
+                                            }
+                                            else {
+                                                $scope.flagsVisibility = 'inherit';
+                                            }
+                                        }, 500);
+                                        //stop the blinking after the CAUTIONBLINKDURATION
+                                        $timeout(function() {
+                                            if ($scope._blink) {
+                                                $interval.cancel($scope._blink);
+                                                $scope.flagsVisibility = 'inherit';
+                                                $scope._blink = null;
+                                                $scope.blink = false;
+                                            }
+                                        },$scope.CAUTIONBLINKDURATION);
+                                    }
+                                }
+                                else
+                                if ($scope._blink) {
+                                    $interval.cancel($scope._blink);
+                                    $scope.flagsVisibility = 'inherit';
+                                    $scope._blink = null;
+                                }
+                            }
+                            // end flag edits
+
                             //load translations
                             sraDispatcher.loadTranslations(sraDispatcher.getWidgetUrl(self.url), 'text', function (path) {
                             $scope.translations = sraDispatcher.getTranslation(path);
                             });
                         }]
                         , link: function ($scope, $element, $attrs) {
+
+                            // start flag edits
+                            /** your code goes here **/
+                            $attrs.sraArgsData += ";Session/Type";
+                            $attrs.sraArgsData += ";Session/Status";
+                            $attrs.sraArgsData += ";Session/Messages";
+                            $attrs.sraArgsData += ";Session/IsGreenFlag";
+                            $attrs.sraArgsData += ";Session/IsCautionFlag";
+                            $attrs.sraArgsData += ";Session/IsRedFlag";
+                            $attrs.sraArgsData += ";Session/IsWhiteFlag";
+                            $attrs.sraArgsData += ";Session/IsCheckeredFlag";
+                            $attrs.sraArgsData += ";Session/IsCrossedFlag";
+                            
+                            $attrs.sraArgsData += ";Car/REFERENCE/Messages";
+                            $attrs.sraArgsData += ";Car/REFERENCE/IsYellowFlag";
+                            $attrs.sraArgsData += ";Car/REFERENCE/IsBlueFlag";
+                            $attrs.sraArgsData += ";Car/REFERENCE/IsBlackFlag";
+                            $attrs.sraArgsData += ";Car/REFERENCE/IsDisqualifyFlag";
+
+                            $scope.$watch("data.Session.Type.Value", $scope.update);
+                            $scope.$watch("data.Session.Status.Value", $scope.update);
+                            $scope.$watch("data.Session.Messages.Value", $scope.update);
+                            $scope.$watch("data.Session.IsGreenFlag.Value", $scope.update);
+                            $scope.$watch("data.Session.IsCautionFlag.Value", $scope.update);
+                            $scope.$watch("data.Session.IsRedFlag.Value", $scope.update);
+                            $scope.$watch("data.Session.IsWhiteFlag.Value", $scope.update);
+                            $scope.$watch("data.Session.IsCheckeredFlag.Value", $scope.update);
+                            $scope.$watch("data.Session.IsCrossedFlag.Value", $scope.update);
+                            $scope.$watch("data.Car.REFERENCE.Messages.Value", $scope.update);
+                            $scope.$watch("data.Car.REFERENCE.IsYellowFlag.Value", $scope.update);
+                            $scope.$watch("data.Car.REFERENCE.IsBlueFlag.Value", $scope.update);
+                            $scope.$watch("data.Car.REFERENCE.IsBlackFlag.Value", $scope.update);
+                            $scope.$watch("data.Car.REFERENCE.IsDisqualifyFlag.Value", $scope.update);
+                            // end flag edits
+
                             $scope.names = sraDispatcher.subscribe($scope, $attrs, self.defaultInterval); //register subscriptions and options to the dispatcher
                         }
                     };
